a180e541fc3f93593474ddfb01ce70f692d7da98
priyabachate <45043739+priyabachate@users.noreply.github.com>
2019-02-07 22:02:29 +0530

    date.java
    final change for practice

--- SimplePractice/src/PAGE/date.java
+++ SimplePractice/src/PAGE/date.java
@@ -4,8 +4,8 @@ public class date {
 
 	public static void main(String[] args) {
 		System.out.println("dd/mm/yy i.e 1/5/19 it is important");
-		System.out.println("make changes in ur code, it is must")
-
+		System.out.println("make changes in ur code, it is must");
+                System.out.println("final change");
 	}
 
 }
