27311b3a6be675691dec29334689f2b4ef80f775
Administrator <Administrator@USER-PC>
2019-02-07 17:45:56 +0530

    date have changes see url also

---
+++ SimplePractice/src/PAGE/date.java
@@ -0,0 +1,10 @@
+package PAGE;
+
+public class date {
+
+	public static void main(String[] args) {
+		System.out.println("dd/mm/yy i.e 1/5/19 it is important");
+
+	}
+
+}
---
+++ SimplePractice/src/PAGE/url.java
@@ -0,0 +1,10 @@
+package PAGE;
+
+public class url {
+
+	public static void main(String[] args) {
+System.out.println("www.github.com go to url");
+
+	}
+
+}
