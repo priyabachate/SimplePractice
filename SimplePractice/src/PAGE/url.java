96ae06e3385894357a8ef2f80409ce199a8ab702
priyabachate <45043739+priyabachate@users.noreply.github.com>
2019-02-07 21:38:33 +0530

    url.java
    add some message check it

--- SimplePractice/src/PAGE/url.java
+++ SimplePractice/src/PAGE/url.java
@@ -3,7 +3,7 @@ package PAGE;
 public class url {
 
 	public static void main(String[] args) {
-System.out.println("www.github.com go to url");
+System.out.println("www.github.com go to url use different url");
 
 	}
 
