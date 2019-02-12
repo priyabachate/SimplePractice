05d75f4abf2f54b6f80ba5e67b7653ab757799f3
Administrator <Administrator@USER-PC>
2019-02-12 13:27:17 +0530

    add date

--- SimplePractice/src/PAGE/date.java
+++ SimplePractice/src/PAGE/date.java
@@ -5,7 +5,7 @@ public class date {
 	public static void main(String[] args) {
 		System.out.println("dd/mm/yy i.e 1/5/19 it is important");
 		System.out.println("make changes in ur code, it is must");
-		System.out.println("12/02/2019");
+		System.out.println("12/02/2020");
 
 	}
 
